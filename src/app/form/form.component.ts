727dba54929b7a247025aecb9c476565455fc74c
majiang <majiang@users.noreply.github.com>
2023-04-08 14:54:40 +0900

    add form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-
+  form = new FormGroup({
+    basic: new FormGroup({
+      one: new FormGroup({
+        a: new FormControl(""),
+        b: new FormControl(""),
+      }),
+      two: new FormControl(""),
+    }),
+    common: new FormGroup({
+      one: new FormControl(""),
+      two: new FormControl(""),
+    }),
+    a: new FormGroup({
+      one: new FormGroup({
+        a: new FormControl(""),
+        b: new FormControl(""),
+      }),
+      two: new FormControl(""),
+    }),
+    b: new FormGroup({
+      one: new FormControl(""),
+      two: new FormControl(""),
+    }),
+  })
 }
